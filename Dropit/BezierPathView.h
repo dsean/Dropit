fecc84cdafe2c389b0aa4308b8f9b3d144e53780
tsean <tsean>
2014-07-04 11:24:04 +0800

    add DezierPathView

---
+++ Dropit/BezierPathView.h
@@ -0,0 +1,15 @@
+//
+//  BezierPathView.h
+//  Dropit
+//
+//  Created by 楊 德忻 on 2014/7/4.
+//  Copyright (c) 2014年 楊 德忻. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BezierPathView : UIView
+
+@property (strong, nonatomic)UIBezierPath *path;
+
+@end
